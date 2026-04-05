3e1952279de95e88bfead1be3f6715338eaa9f0a
Rayhan <lahdjirayhan@users.noreply.github.com>
2026-04-05 23:30:34 +0700

    Change placement and formatting of address

--- src/js/time.js
+++ src/js/time.js
@@ -4,17 +4,17 @@ export const time = () => {
     const timeContainer = document.querySelector('.time');
     const [marriageDiv, receptionDiv] = timeContainer.querySelectorAll('div div');
     const mapLink = timeContainer.querySelector('a');
-    const addressParagraph = timeContainer.querySelector('a + p');
+    const addressParagraph = timeContainer.querySelector('div + p');
 
     const createTimeListItem = (title, details) => (
         `<h3>${title}</h3>
-         <p>${details.day}, ${details.date} ${details.month} ${details.year} <br> 
-         Pukul ${details.hours.start} WIB sd ${details.hours.finish}</p>`
+         <p>${details.day}, ${details.date} ${details.month} ${details.year} <br>
+         Pukul ${details.hours.start}${details.hours.finish ? ` sd ${details.hours.finish}`: ''} WIB</p>`
     );
 
     marriageDiv.innerHTML = createTimeListItem('Akad', data.time.marriage);
     receptionDiv.innerHTML = createTimeListItem('Resepsi', data.time.reception);
 
     mapLink.href = data.link.map;
-    addressParagraph.textContent = data.time.address;
+    addressParagraph.innerHTML = data.time.address.split('|').map(s => s.trim()).filter(s => s).join('<br>');
 };
