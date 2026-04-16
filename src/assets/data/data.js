80e16c182440dcce3203815d8cddcfd21e3da703
Rayhan <lahdjirayhan@users.noreply.github.com>
2026-04-16 20:28:52 +0700

    Add spreadsheet and script backend API

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -89,7 +89,7 @@ export const data = {
 
     audio: './src/assets/audio/wedding.mp3',
 
-    api: 'https://script.google.com/macros/s/AKfycbyydz6N4p2VWUG8zsXeURv6ap9RP8a4eC3x6N3x6qTDjMVr1cIBz9S0NsHw2rWvBOSXGg/exec',
+    api: 'https://script.google.com/macros/s/AKfycbwDamk6T1IL4D0Gs9WA_WWbBf1TxclCXmwN_9FrZ6QcKygYEw1tIs6UzahMQvvnE0I7Xw/exec',
 
     navbar: [
         {
